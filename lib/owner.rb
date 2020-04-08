17633c01fdee9a520a214c9a03450a4b15343b2f
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 17:49:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
   # code goes here
+  @@all = []
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.count
+  end
+  def self.reset_all
+    @@all = []
+  end
+  def say_species
+    return "I am a human."
+  end
 end
